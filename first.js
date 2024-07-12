2646c390e81ef02332e7038bd24c8ead5cc3a872
Amir zada <zadaa3322@gmail.com>
2024-07-12 22:10:04 +0500

    initial comit

---
+++ first.js
@@ -0,0 +1,24 @@
+
+
+// let x=1;
+// if(x>=1){
+//     console.log('this is true');
+// }
+// else{
+//     console.log('this is false')
+// }
+// console.log(typeof x);
+// let mark=[1,4,5,65,6];
+// console.log(typeof mark);
+let x={
+    name:'amir',
+    roll:'234',
+    dob:3334,
+    marital_state:'marreid',
+    car:['oide','finla','toyota']
+}
+
+console.log(x.car[0]);
+console.log(Object.values(x).length);
+
+
